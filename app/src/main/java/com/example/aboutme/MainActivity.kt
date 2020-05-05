bd3047d6721f818da9bac893532b0203a4b4df3c
Graziano Felline <graziano.felline@adcubum.com>
2020-05-05 16:03:45 +0200

    7. Task: Add a click listener to the DONE button

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        done_button.setOnClickListener {
+            addNickname(it)
+            hideKeyboard(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        nickname_text.text = nickname_edit.text
+        nickname_edit.visibility = View.GONE
+
+        view.visibility = View.GONE
+
+        nickname_text.visibility = View.VISIBLE
+    }
+
+    private fun hideKeyboard(it: View) {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
     }
 }
